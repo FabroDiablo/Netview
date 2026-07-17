78431793bdebff9e0ff7bfdc3bf9ff9c455150e7
EC2 Default User <ec2-user@ip-172-31-37-137.us-east-2.compute.internal>
2026-07-17 09:11:53 +0000

    Remove db.php from tracking, add to gitignore

--- yii2basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=db;dbname=netview',
-    'username' => 'yii2',
-    'password' => 'yii2password',
-    'charset'  => 'utf8',
-];
